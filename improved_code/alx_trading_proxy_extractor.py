7a337f6254733062f3ede20006d736b748689cad
Drxmiiz <beamr.1232@gmail.com>
2025-05-27 20:35:53 +0000

    feat: Refactor proxy extractor setup and add comprehensive data extraction reports

--- improved_code/alx_trading_proxy_extractor.py
+++ improved_code/alx_trading_proxy_extractor.py
@@ -1,3 +1,7 @@
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.error_handler import safe_execution, safe_print
 
 #!/usr/bin/env python3
