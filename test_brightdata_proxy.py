c833d8b00e3a54a59da3e9c47f9d7294e27a4442
Drxmiiz <beamr.1232@gmail.com>
2025-05-28 19:21:41 +0000

    Refactor code structure for improved readability and maintainability

--- test_brightdata_proxy.py
+++ test_brightdata_proxy.py
@@ -9,10 +9,12 @@ brd_passwd = 'q9yihckpz3qk'
 brd_superpoxy = 'brd.superproxy.io:33335'
 brd_connectStr = 'brd-customer-' + brd_user + '-zone-' + brd_zone + ':' + brd_passwd + '@' + brd_superpoxy
 
-brd_test_url = 'https://geo.brdtest.com/welcome.txt'
+brd_test_url = 'https://geo.brdtest.com/mygeo.json'
 
-# Skip SSL verification for test
-context = ssl._create_unverified_context()
+
+# Use the downloaded CA certificate for SSL verification
+ca_cert_path = 'brightdata_ca/New SSL certifcate - MUST BE USED WITH PORT 33335/BrightData SSL certificate (port 33335).crt'
+context = ssl.create_default_context(cafile=ca_cert_path)
 
 opener = urllib.request.build_opener(
     urllib.request.ProxyHandler(
